b76e2abf9493b9c745fda9f919514401ae350243
millod <milad.everlast@gmail.com>
2021-07-06 15:30:11 +0430

    Add files via upload

---
+++ keywords/js/key.js
@@ -0,0 +1,119 @@
+
+$(document).ready(function () {
+    $('#RightMenuView #menu-news-manage').addClass('mSelected');
+    var logoSrc = $('#RightMenuView #menu-news-manage .mImg').attr('src');
+    $('#RightMenuView #menu-news-manage .mImg').attr('src', logoSrc.replace('gray', 'color'));
+    if ($.cookie('newsRoomRMenuPos') == null) $.cookie('newsRoomRMenuPos', '0px 235px', { path: '/' });
+    var menuPosOld = $.cookie('newsRoomRMenuPos');
+    var menuPosNew = '0px 235px';
+    $('.rightpanHolder').css('backgroundPosition', menuPosOld);
+    $('.rightpanHolder').animate({ backgroundPosition: menuPosNew }, 800);
+    $.cookie('newsRoomRMenuPos', menuPosNew, { path: '/' });
+    $("#codeStrip").append($('.sub-menu-news-manage'));
+
+
+    $('#txtKeywordSearch').keydown(function (e) {
+        if (e.keyCode == 13) e.preventDefault();
+    });
+
+    $('#txtKeywordSearch').keyup(function (e) {
+        moreKeywords = 15;
+        if ($.trim($('#txtKeywordSearch').val()).length > 1) {
+            keywordWatcher();
+            if (e.keyCode == 13)
+            {
+                e.preventDefault();
+                $('#searchedKeywords .keywordItem').each(function () {
+                    if ($(this).text() == $('#txtKeywordSearch').val())
+                    {
+                        $(this).click();
+                        return false;
+                    }
+                });
+            }
+            return false;
+        }
+    });
+
+    $('#btnKeywordSearch').click(function () {
+        moreKeywords = 50;
+        keywordWatcher();
+    });
+    $('#btnKeywordSearchAll').click(function () {
+        moreKeywords = -1;
+        keywordWatcher();
+    });
+
+    $('#selectedKeywords .keywordItem').click(function () {
+        $(this).remove();
+        setValueOfKeywords();
+    });
+
+    $("#btnNewKeySave").click(function () {
+        var searchKey = $.trim($('#txtKeywordSearch').val());
+        $.ajax({
+            url: $('#keywordPlace').attr('addkeyurl'),
+            data: "searchKey=" + searchKey,
+            dataType: "html",
+            cache: "false",
+            context: document.body,
+            type: "POST",
+            success: function (result) {
+                $('#searchedKeywords').html(result);
+                $('#selectedKeywords .keywordItem').each(function () {
+                    $('#searchedKeywords .keywordItem[keyID=' + $(this).attr('keyID') + ']').remove();
+                });
+                setClickForAddKeys();
+            }
+        });
+    });
+    setValueOfKeywords();
+});
+
+var moreKeywords = 15;
+function keywordWatcher() {
+        clearTimeout($.data(this, 'timerkeyword'));
+        var wait = setTimeout(searchKeyword, 500);
+        $(this).data('timerkeyword', wait);
+}
+
+function searchKeyword() {
+    var searchKey = $.trim($('#txtKeywordSearch').val());
+    if (searchKey.length > 1) {
+        $.ajax({
+            url: $('#keywordPlace').attr('searchkeyurl'),
+            data: "searchKey=" + searchKey + "&Top=" + moreKeywords,
+            dataType: "html",
+            cache: "false",
+            context: document.body,
+            type: "POST",
+            success: function (result) {
+                $('#searchedKeywords').html(result);
+                $('#selectedKeywords .keywordItem').each(function () {
+                    $('#searchedKeywords .keywordItem[keyID=' + $(this).attr('keyID') + ']').hide();
+                });
+                setClickForAddKeys();
+            }
+        });
+    }
+}
+
+function setClickForAddKeys() {
+    $('.keywordItem').click(function () {
+        if ($.trim($(this).parent().attr('id')) == 'searchedKeywords') {
+            $('#selectedKeywords').append(this);
+        }
+        else {
+            $(this).remove();
+        }
+        setValueOfKeywords();
+    });
+}
+function setValueOfKeywords() {
+    var keyVals = '';
+    $('#selectedKeywords .keywordItem').each(function () {
+        keyVals += $(this).attr('keyID') + ',';
+    });
+    $('#keywordIdsVal').val(keyVals);
+
+}
